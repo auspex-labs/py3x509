5d247f03e1f4619da4143bd1317c8d06f77b36c3
CJ Niemira <siege@siege.org>
2017-02-08 16:59:49 -0500

    print

--- x509/pkcs7/debug.py
+++ x509/pkcs7/debug.py
@@ -23,7 +23,7 @@ Created on Dec 9, 2009
 def show_bytes(string):
     print('--------------')
     for byte in string:        
-        print hex(ord(byte)),
+        print(hex(ord(byte)))
     print('\n--------------')
 
 def write_to_file(what, where):
